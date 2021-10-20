8fed4fe0fe0cb790db1f1051f2fdb66b804de69a
Zsolt Medgyesi <zmedgyes@users.noreply.github.com>
2021-10-20 23:34:26 +0200

    setup redis session store

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const session = require('express-session');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+
 const app = express();
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient(process.env.REDIS_URL);
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
+//Configure session middleware
+app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: '21dn57aebd',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie 
+        maxAge: 1000 * 60 * 10 // session max age in miliseconds
+    }
+}));
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
