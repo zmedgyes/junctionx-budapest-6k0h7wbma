ae6f70fca521fca505e2df49beccfcaa71e3f372
Zsolt Medgyesi <zmedgyes@gmail.com>
2021-10-21 20:51:25 +0200

    frontend routes, error handling

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 const { getInjector } = require('./config/injector-config');
 const { initMiddlewares } = require('./config/middleware-config');
-const { initRoutes } = require('./config/router-config');
 const { initApp } = require('./config/app-config');
 
 const injector = getInjector();
 
 initMiddlewares(injector);
-initRoutes(injector);
 initApp(injector);
--- backend/routes/basic.js
+++
@@ -1,7 +0,0 @@
-module.exports = (injector) => {
-    const app = injector.get('app');
-
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
-};
