c17a214b7d0187c54b34eaecc5a0a7fb67075d1e
bubment <medgyesiattila@elektromont.hu>
2021-10-22 15:27:14 +0200

    remove google default map UI items

--- frontend/src/constants/mapConfig.js
+++ frontend/src/constants/mapConfig.js
@@ -3,7 +3,8 @@ const mapStyles = require("./mapStyles")
 const mapConfig = {
     zoom: 4,
     center: { lat: -25.344, lng: 131.036 },
-    styles: mapStyles.defaultTestStyle
+    styles: mapStyles.defaultTestStyle,
+    disableDefaultUI: true,
 }
 
 module.exports = mapConfig;
