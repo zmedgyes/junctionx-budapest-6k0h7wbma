9895d18738a0a6e5846d3ae7fb64da6d1c25abe7
bubment <medgyesiattila@elektromont.hu>
2021-10-22 21:43:43 +0200

    refactor map structure

--- frontend/src/views/GoogleMap.vue
+++ frontend/src/views/GoogleMap.vue
@@ -1,19 +1,11 @@
 <template>
-    <GoogleMapLoader
-    :mapConfig="mapDefaultConfig"
-    :apiKey="googleApiKey"
-    :initialMarkers="markers"
-    />
-    <button @click="removeMarker(2)">Remove second marker</button>
-    <button @click="getChallangeList()">Get challange list</button>
+    <GoogleMapLoader ref="mapLoader" :userId="userId"/>
+    <button @click="$refs.mapLoader.removeMarker(2)">Remove second marker</button>
     <router-link tag="button" :to="{name : 'Home'}">Home</router-link>
 </template>
 
 <script>
 import GoogleMapLoader from '../components/GoogleMapLoader.vue'
-import mapDefaultConfig from "../constants/mapConfig"
-import { googleApiKey } from "../config/index"
-import { listChallenges } from "../remotes/remotes"
 
 export default {
     name: 'GoogleMap',
@@ -22,24 +14,11 @@ export default {
     },
     data(){
         return{
-            mapDefaultConfig,
-            googleApiKey,
-            markers: [
-                {position:{ lat: 47.4858873, lng: 19.077491 }, type:"you-are-here", id:1},
-                {position:{ lat: 47.4838873, lng: 19.075491 }, type:"treasure", id:2},
-                {position:{ lat: 47.4848873, lng: 19.076491 }, type:"treasure", id:3},
-                {position:{ lat: 47.4848873, lng: 19.078491 }, type:"treasure", id:4},
-            ]
+            userId:1,
         }
     },
     methods:{
-        removeMarker(markerId){
-            this.markers = this.markers.filter(marker => marker.id != markerId)
-        },
-        async getChallangeList(){
-            let result = await listChallenges(1)
-            console.log(result)
-        }
+
     },
 
 }
