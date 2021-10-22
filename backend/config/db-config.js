c8b0c6a1da3abe95414cfa2c91d908ca4fc819d0
Zsolt Medgyesi <zmedgyes@users.noreply.github.com>
2021-10-22 13:13:00 +0200

    basic user model, basic auth

--- backend/config/db-config.js
+++ backend/config/db-config.js
@@ -1,10 +1,20 @@
 module.exports = { 
     initDB: async (injector) => {
         const db = injector.get('db');
-
+        
+        // EXAMPLE
         await db.query(`CREATE TABLE IF NOT EXISTS example (
                 example_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 value TEXT NOT NULL
+            )`);
+
+        // USER
+        await db.query(`CREATE TABLE IF NOT EXISTS user (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                password_hash TEXT NOT NULL,
+                roles JSON NOT NULL,
+                data JSON NOT NULL
             )`);
     }
 };
