046a9c835e1ebebe0dc761038068b60859f37046
Zsolt Medgyesi <zmedgyes@users.noreply.github.com>
2021-10-22 11:48:31 +0200

    simplify routing

--- backend/config/router-config.js
+++ backend/config/router-config.js
@@ -1,9 +1,20 @@
-const exampleRoutes = require('../routes/example-routes');
-const staticRoutes = require('../routes/static-routes');
+const path = require('path');
+const express = require('express');
+const { USER_ROLES } = require('../misc/auth-util');
 
 module.exports = {
     initRoutes: (injector) => {
-        exampleRoutes(injector);
-        staticRoutes(injector);
+        const routeHelper = injector.get('routeHelper');
+
+        // EXAMPLE
+        routeHelper.addAPIRoute('get', '/public/example', 'exampleController', 'getExamples');
+        routeHelper.addAPIRoute('get', '/user/example', 'exampleController', 'getExamples', USER_ROLES.USER);
+        routeHelper.addAPIRoute('get', '/admin/example', 'exampleController', 'getExamples', USER_ROLES.ADMIN);
+
+        // FRONTEND
+        const app = injector.get('app');
+        const frontendPath = path.resolve(`${__dirname}/../../frontend/dist`);
+        app.use(express.static(frontendPath));
+        app.route('/*').get((req, res) => { res.sendFile(path.join(frontendPath, 'index.html')); });
     }
 };
--- backend/routes/example-routes.js
+++
@@ -1,9 +0,0 @@
-const { USER_ROLES } = require("../misc/auth-util");
-
-module.exports = (injector) => {
-    const routeHelper = injector.get('routeHelper');
-
-    routeHelper.addAPIRoute('get', '/public/example', 'exampleController', 'getExamples');
-    routeHelper.addAPIRoute('get', '/user/example', 'exampleController', 'getExamples', USER_ROLES.USER);
-    routeHelper.addAPIRoute('get', '/admin/example', 'exampleController', 'getExamples', USER_ROLES.ADMIN);
-};
