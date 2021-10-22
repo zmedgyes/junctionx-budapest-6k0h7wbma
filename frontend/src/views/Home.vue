1cb3d233893100e5aaa4a3360ba817d33350424f
z2s8 <szanto.zsombor@gmail.com>
2021-10-22 15:08:20 +0200

    Add net card

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,50 @@
 <template>
-  <div class="title-container">
-      <h1>Consolidity trialtask solution</h1>
-      <h2>by Medgyesi Attila</h2>
+  <div class="home">
+      <div class="titlebar">
+        <VodaLogo size="40px" />
+        <div>Welcome back, XYZ!</div>
+      </div>
+      <div class="homeContent">
+        <div class="cardRow">
+          <NetCard/>
+          <Card width="50%"><h1>Hi</h1></Card>
+          <Card width="50%"><h1>Hi</h1></Card>
+          <Card width="50%"><h1>Hi</h1></Card>
+          <Card width="50%"><h1>Hi</h1></Card>
+        </div>
+        <div class="cardRow">
+          <Card><h1>Hi</h1></Card>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-
+import VodaLogo from '../components/VodaLogo.vue'
+import Card from '../components/Card.vue'
+import NetCard from '../components/cards/NetCard.vue'
 
 export default {
     name: 'Home',
+    components: { VodaLogo, Card, NetCard }
 }
 </script>
 <style scoped>
-.title-container{
-  margin: 4rem 4rem;
+.cardRow {
+  display: flex;
+  flex-wrap: wrap;
+}
+.homeContent {
+  padding: 5px 20px;
+}
+.titlebar {
+    height: 75px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px 20px;
+    font-size: 20px;
 }
 </style>
 
