435afcdeb2f3e6bf36d535329a1ecd9675ae2834
z2s8 <szanto.zsombor@gmail.com>
2021-10-22 20:32:02 +0200

    Add QRscan overlay to map

--- frontend/src/views/GoogleMap.vue
+++ frontend/src/views/GoogleMap.vue
@@ -1,12 +1,16 @@
 <template>
-    <GoogleMapLoader
-    :mapConfig="mapDefaultConfig"
-    :apiKey="googleApiKey"
-    :initialMarkers="markers" 
-    />
+    <div>
+        <QRScan v-if="!QRclosed" @closeQR="onCloseQR" />
+        <GoogleMapLoader
+        :mapConfig="mapDefaultConfig"
+        :apiKey="googleApiKey"
+        :initialMarkers="markers" 
+        />
+    </div>
 </template>
 
 <script>
+import QRScan from '../components/QRScan.vue'
 import GoogleMapLoader from '../components/GoogleMapLoader.vue'
 import mapDefaultConfig from "../constants/mapConfig"
 import { googleApiKey } from "../config/index"
@@ -14,7 +18,7 @@ import { googleApiKey } from "../config/index"
 export default {
     name: 'GoogleMap',
     components: {
-        GoogleMapLoader
+        GoogleMapLoader, QRScan
     },
     data(){
         return{
@@ -24,7 +28,13 @@ export default {
                 {position:{ lat: 47.4858873, lng: 19.077491 }, type:"you-are-here", id:1},
                 {position:{ lat: 47.4838873, lng: 19.075491 }, type:"treasure", id:2},
                 {position:{ lat: 47.4848873, lng: 19.076491 }, type:"treasure", id:3},
-            ]
+            ],
+            QRclosed: false
+        }
+    },
+    methods: {
+        onCloseQR() {
+            this.QRclosed = true
         }
     }
 }
