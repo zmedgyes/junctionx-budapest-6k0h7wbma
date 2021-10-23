21db2e4513aecc262fbf1fcef403b5c240987352
z2s8 <szanto.zsombor@gmail.com>
2021-10-23 13:03:20 +0200

    Add pointshop

---
+++ frontend/src/components/cards/PointsBalanceCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <Card width="100%">
+    <div class="cardTitle"><img src="/img/vodapoint.svg"/> Voda Point Balance</div>
+    <div class="cardContent">
+      <div><span class="big">35</span> <span class="bigish">Pts</span></div>
+      <div class="due">available until: 2021.12.11.</div>
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from '../Card.vue'
+
+export default {
+  components: {Card}
+}
+</script>
+
+
+<style scoped>
+  .cardTitle img {
+    height: 18px;
+    width: 18px;
+    margin-bottom: 5px;
+  }
+  .cardContent {
+    font-size: 25px;
+  }
+  .cardContent .big {
+    font-size: 35px;
+    font-weight: 500;
+    margin-left: 20px;
+  }
+
+  .cardContent .bigish {
+    font-size: 30px;
+    font-weight: 500;
+  }
+
+  .due {
+    font-size: 17px;
+    color: var(--brand-lightgrey);
+    text-align: end;
+  }
+
+  .mybills {
+    font-size: 20px;
+  }
+</style>
