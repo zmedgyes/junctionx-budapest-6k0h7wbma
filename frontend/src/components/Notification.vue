07d439a904ff92cc941d0fe80ccfd6118f475f4b
bubment <medgyesiattila@elektromont.hu>
2021-10-24 00:36:36 +0200

    temporaty streak notification

---
+++ frontend/src/components/Notification.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.notification{
+    z-index: 5000;
+    position: fixed;
+    top:5px;
+    left:5%;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,6 +6,12 @@
       </div>
       <div class="homeContent">
         <div class="cardRow">
+          <Card
+          bg="#f59999" style="text-align:center" class="streak-notification" v-if="isNotificationOn">
+            <span style="color:white; font-weight:bold">1st day streak is pretty nice!</span>
+          </Card>
+        </div>
+        <div class="cardRow">
           <NetCard
           :dataBalance="dataBalance" />
           <BillCard />
@@ -41,21 +47,32 @@ export default {
     data(){
         return{
           dataBalance:null,
+          isNotificationOn:true
         }
     },
     async mounted(){
       this.$store.dispatch('activateLoader')
       await User.initUser(1);
       this.dataBalance = User.data.dataBalance
-      let randomLoadingTime = Math.floor((Math.random()*2000)+800);
+      let randomLoadingTime = Math.floor((Math.random()*1300)+400);
       setTimeout(() =>{ 
         this.$store.dispatch('deActivateLoader')
        }, randomLoadingTime);
+       setTimeout(()=>{
+         this.isNotificationOn = false
+        }, 3000);
     }
   }
 
 </script>
 <style scoped>
+.notification{
+    z-index: 5000;
+    position: fixed;
+    top: 1%;
+    left: 2%;
+    width: 96vw;
+}
 .cardRow {
   display: flex;
   flex-wrap: wrap;
@@ -87,5 +104,37 @@ h2 {
   flex-direction: column;
   width: 50%;
 }
+
+/* .streak-notification {
+  animation: fadeIn 5s;
+  -webkit-animation: fadeIn 5s;
+  -moz-animation: fadeIn 5s;
+  -o-animation: fadeIn 5s;
+  -ms-animation: fadeIn 5s;
+}
+@keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-moz-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-webkit-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-o-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-ms-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+} */
 </style>
 
